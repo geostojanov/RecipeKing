b3cf47b69de21b134603d7ca2738c81261311277
geostojanov <gestojanov@gmail.com>
2017-09-06 20:15:51 +0200

    Implement search button functionality.

--- app/src/main/java/recipeking/uw/tacoma/edu/recipeking/CategoriesActivity.java
+++ app/src/main/java/recipeking/uw/tacoma/edu/recipeking/CategoriesActivity.java
@@ -30,13 +30,12 @@ public class CategoriesActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Button will be implemented in next phase.
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+            onSearchRequested();
             }
         });
 
